8701b1a1a85cb9cfdb5eb6f7783552d71766ea6f
theoarvin <arvintheo69@gmail.com>
2022-05-19 21:19:54 +0200

    premier commit

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+// importation de express
+const express = require('express')
+
+// importation du fichier database pour mongoose
+const mongoose = require('./db/db');
+
+const app = express();
+
+
+
+
+  
+  
+module.exports = app;
